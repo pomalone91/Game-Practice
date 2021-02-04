c210b35d68a0bf2b9f3d8097af419a950df345a2
pomalone91 <pomalone91@gmail.com>
2021-02-03 18:41:43 -0700

    Initial Commit

---
+++ Game Practice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Game Practice
+//
+//  Created by Paul Malone on 2/3/21.
+//  Copyright © 2021 Paul Malone. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
